310a5e82e6cfd57504b02216a9ceb8a789fa21f2
Michael van Nes <mvannes@hostnet.nl>
2022-12-11 15:40:55 +0100

    Day ten part two

--- ten/main.go
+++ ten/main.go
@@ -15,37 +15,50 @@ func main() {
 		log.Fatalln(err.Error())
 	}
 	s := bufio.NewScanner(f)
+
+	var screen [6][40]string
+
 	cycleCount := 0
 	register := 1
 	addRegex := regexp.MustCompile("addx ([-0-9]+)")
-
-	cycleToPrint := 20
-	var sumCycles int
 	for s.Scan() {
 		cycleCount++
-
-		if cycleToPrint == cycleCount {
-			sumCycles += cycleCount * register
-			cycleToPrint += 40
-		}
+		draw(cycleCount, &screen, register)
 
 		line := s.Text()
 		if line == "noop" {
 			continue
 		}
-
 		cycleCount++
-		if cycleToPrint == cycleCount {
-			sumCycles += cycleCount * register
-			cycleToPrint += 40
-		}
+
 		addMatch := addRegex.FindStringSubmatch(line)
 		registerAdd, err := strconv.Atoi(addMatch[1])
 		if err != nil {
 			log.Fatal(err.Error())
 		}
 		register += registerAdd
+		draw(cycleCount, &screen, register)
+
 	}
 
-	fmt.Println(sumCycles)
+	for _, r := range screen {
+		fmt.Println(r)
+	}
+}
+
+func draw(cycleCount int, screen *[6][40]string, register int) {
+	rowNumber := ((cycleCount - 1) / 40)
+	rowPosition := cycleCount % 40
+
+	var absReg int
+	if rowPosition >= register {
+		absReg = rowPosition - register
+	} else {
+		absReg = register - rowPosition
+	}
+	if absReg <= 1 {
+		screen[rowNumber][rowPosition] = "#"
+	} else {
+		screen[rowNumber][rowPosition] = "."
+	}
 }
