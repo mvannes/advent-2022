f0df4e0085f59a260c2bfb0b57850d2a2bf7ea7e
Michael van Nes <mvannes@hostnet.nl>
2022-12-13 09:28:36 +0100

    Day twelve part two

--- twelve/main.go
+++ twelve/main.go
@@ -22,8 +22,46 @@ type Edge struct {
 	Weight int
 }
 
+type PrioNode struct {
+	node   *Node
+	weight int
+	prevId int
+}
+
+type PrioQueue struct {
+	nodes []PrioNode
+}
+
+func (p *PrioQueue) addNode(n *Node, weight int, prevId int) {
+	p.nodes = append(p.nodes, PrioNode{
+		node:   n,
+		weight: weight,
+		prevId: prevId,
+	})
+	sort.Slice(p.nodes, func(a, b int) bool {
+		return p.nodes[b].weight > p.nodes[a].weight
+	})
+}
+
+func (p *PrioQueue) hasMore() bool {
+	return len(p.nodes) != 0
+}
+
+func (p *PrioQueue) pop() PrioNode {
+	res := p.nodes[:1]
+	p.nodes = p.nodes[1:]
+	return res[0]
+}
+
 func calcWeight(a, b *Node) int {
-	if b.Elevation-1 > a.Elevation {
+
+	// if only may go up one, but down any
+	//if b.Elevation-1 > a.Elevation {
+	//	return math.MaxInt64
+	//}
+
+	// if only may go down one, but up any.
+	if a.Elevation-1 > b.Elevation {
 		return math.MaxInt64
 	}
 	return 1
@@ -81,87 +119,72 @@ func main() {
 		}
 		lineCount++
 	}
-	calculateTraveled(nodeSlice, "S")
+	calculateTraveled(nodeSlice, "E", "a")
 }
 
-type PrioNode struct {
-	node   *Node
-	weight int
-	prevId int
-}
+func calculateTraveled(nodes []*Node, startCharacter string, endCharacter string) {
+	var startNodes []*Node
 
-type PrioQueue struct {
-	nodes []PrioNode
-}
-
-func (p *PrioQueue) addNode(n *Node, weight int, prevId int) {
-	p.nodes = append(p.nodes, PrioNode{
-		node:   n,
-		weight: weight,
-		prevId: prevId,
-	})
-	sort.Slice(p.nodes, func(a, b int) bool {
-		return p.nodes[b].weight > p.nodes[a].weight
-	})
-}
-
-func (p *PrioQueue) hasMore() bool {
-	return len(p.nodes) != 0
-}
-
-func (p *PrioQueue) pop() PrioNode {
-	res := p.nodes[:1]
-	p.nodes = p.nodes[1:]
-	return res[0]
-}
-
-func calculateTraveled(nodes []*Node, characterToFind string) {
-	var endNode *Node
-	var visited = map[int]bool{}
-	var dists = map[int]int{}
 	for _, n := range nodes {
-		if n.Char == characterToFind {
-			endNode = n
+		if n.Char == startCharacter {
+			startNodes = append(startNodes, n)
 		}
-
-		dists[n.id] = math.MaxInt64
 	}
-	pq := PrioQueue{}
-	pq.addNode(endNode, 0, -1)
 
-	for pq.hasMore() {
-		node := pq.pop()
-		id := node.node.id
-		if visited[id] {
-			continue
-		}
-		visited[id] = true
+	var results []int
+	fmt.Println(len(startNodes))
 
-		prevCost := 0
-		if node.prevId != -1 && dists[node.prevId] != math.MaxInt64 {
-			prevCost = dists[node.prevId]
+	for _, startNode := range startNodes {
+		var visited = map[int]bool{}
+		var dists = map[int]int{}
+		for _, n := range nodes {
+			dists[n.id] = math.MaxInt64
 		}
 
-		if currentDist, ok := dists[id]; !ok {
-			dists[id] = node.weight
-		} else {
-			if currentDist > node.weight {
+		pq := PrioQueue{}
+		pq.addNode(startNode, 0, -1)
+
+		for pq.hasMore() {
+			node := pq.pop()
+			id := node.node.id
+			if visited[id] {
+				continue
+			}
+			visited[id] = true
+
+			prevCost := 0
+			if node.prevId != -1 && dists[node.prevId] != math.MaxInt64 {
+				prevCost = dists[node.prevId]
+			}
+
+			if currentDist, ok := dists[id]; !ok {
 				dists[id] = node.weight
+			} else {
+				if currentDist > node.weight {
+					dists[id] = node.weight
+				}
+			}
+			dists[id] += prevCost
+
+			for _, e := range node.node.Edges {
+				pq.addNode(e.Node, e.Weight, id)
 			}
 		}
-		dists[id] += prevCost
 
-		for _, e := range node.node.Edges {
-			pq.addNode(e.Node, e.Weight, id)
-		}
-	}
+		smallestPath := math.MaxInt64
+		for _, n := range nodes {
+			if n.Char == endCharacter {
+				if smallestPath > dists[n.id] && dists[n.id] > 0 {
 
-	for _, n := range nodes {
-		if n.Char == "E" {
-			fmt.Println(dists[n.id])
+					fmt.Println(dists[n.id])
+					smallestPath = dists[n.id]
+				}
+
+				results = append(results, dists[n.id])
+			}
 		}
+		fmt.Println(smallestPath)
 	}
-	fmt.Println("done!")
 
 }
 
